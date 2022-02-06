01a321c5d1d58c726b7c9791946b9fa5502dc848
John sanjay <disisanjayson@gmail.com>
2022-02-06 22:56:22 +0530

    Day 10 submission

---
+++ Day 10 mng 4thfeb 2022assignment/Day 10 mng 4thfeb 2022assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10_mng_4thfeb_2022assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
