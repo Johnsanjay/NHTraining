a1ce4c2150d7844c49a1a02e9669e57ec8fcb77b
Johnsanjay <98379054+Johnsanjay@users.noreply.github.com>
2022-02-22 15:01:59 +0530

    Add files via upload

---
+++ SanjayFinalProject/BusinessLogicLibrary/EmployeeBLl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary;
+
+
+namespace BusinessLogicLibrary
+{
+    public class EmployeeBLl
+    {
+        public static bool AddEmployee(int empId, string empName, int empSalary, int empAge)
+        {
+            var result=EmployeeDAL.AddEmployee(empId, empName, empSalary, empAge);  
+            return result;
+        }
+        public static List<string> GetEmployeeById(int id)
+        {
+            var result = EmployeeDAL.GetEmployeeById(id);
+            return result;
+        }
+        public static List<string> GetEmployeeByName(string name)
+        {
+            var result = EmployeeDAL.GetEmployeeByName(name);
+            return result;
+        }
+        public static string[] GetAllEmployees()
+        {
+            var result = EmployeeDAL.GetAllEmployees();
+            return result;
+        }
+    }
+}
